7f1df9c62418500e475fa328eb35116ddb934f6e
o4tapabrato <www.tapabratomaity@gmail.com>
2026-09-19 22:39:19 +0530

    Fixed api issue

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -1,7 +1,7 @@
 import { destroySession } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request) {
     try {
         await destroySession();
     }
