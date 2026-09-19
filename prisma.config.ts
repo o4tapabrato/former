9f7c265f1f37aa7fb82a9f8b2b50d41b5141c016
o4tapabrato <www.tapabratomaity@gmail.com>
2026-09-19 23:04:45 +0530

    fixed prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from '@prisma/config';
 export default defineConfig({
     schema: "./prisma/schema.prisma",
     datasource: {
-        url: process.env.DATABASE_URI
+        url: process.env.DATABASE_URI || ""
     }
 });
